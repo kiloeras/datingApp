71ce16bc2087b60f619a9369c264540515b75b75
Miguel Angel Morales Gonzalez <miguelmg1@hotmail.com>
2019-01-22 19:16:36 +0100

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace Dating.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
